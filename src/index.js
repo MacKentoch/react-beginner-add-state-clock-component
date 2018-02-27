c1a367dcebf23edd437705e430efb42cdf43dd25
Erwan DATIN <MacKentoch@users.noreply.github.com>
2018-02-27 13:15:17 +0100

    all steps done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  render(element, document.getElementById('root'));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      now: new Date().toLocaleTimeString(),
+    };
+
+    // tick sera un callback:
+    // avec bind(): on s'assure que this reste défini à Clock
+    // même dans un callback
+    this.tick = this.tick.bind(this);
+
+    // this.tick sera appelé en callback à setInterval()
+    setInterval(this.tick, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.now}.</h2>
+      </div>
+    );
+  }
+
+  tick() {
+    this.setState({ now: new Date().toLocaleTimeString() });
+  }
 }
 
-setInterval(tick, 1000);
+render(<Clock />, document.getElementById('root'));
